ff436ff9f46504f2027fd637ec8bf1b95912f112
andrewww8711 <38190824+andrewww8711@users.noreply.github.com>
2018-04-15 09:57:55 -0700

    added all in one repository

--- group.py
+++ group.py
@@ -1,7 +1,10 @@
 class Group:
-
-    def __init__(self, name, header, footer):
-        self.name = name
-        self.header = header
-        self.footer = footer
-
+    def __init__(self, firstname, lastname, title, company, address, homephone, cellphone, email):
+        self.firstname = firstname
+        self.lastname = lastname
+        self.title = title
+        self.company = company
+        self.address = address
+        self.homephone = homephone
+        self.cellphone = cellphone
+        self.email = email
