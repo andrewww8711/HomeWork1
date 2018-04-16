7727f204f0214096284bf30755a36981ee900eb5
Andrey <avizovskiy@yahoo.com>
2018-04-15 17:54:01 -0700

    Created  group class

--- test/homeworkone.py
+++ test/homeworkone.py
@@ -14,11 +14,11 @@ def app(request):
 
 def test_homeworkone(app):
         app.session.login(username="admin", password="secret")
-        app.create_group(Group(name="homework1", header="homework12", footer="homework13"))
+        app.group.create(Group(name="homework1", header="homework12", footer="homework13"))
         app.session.logout()
 
 
 def test_empty_group_homeworkone(app):
         app.session.login(username="admin", password="secret")
-        app.create_group(Group(name="", header="", footer=""))
+        app.group.create(Group(name="", header="", footer=""))
         app.session.logout()
