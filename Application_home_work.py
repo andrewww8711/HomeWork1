320d385edbd8dc14604f9a35668291da9e1e8e7b
Andrey <avizovskiy@yahoo.com>
2018-04-16 22:46:14 -0700

    Changed to contact parameter

--- Application_home_work.py
+++ Application_home_work.py
@@ -12,34 +12,34 @@ class Application_home:
         wd = self.wd
         wd.find_element_by_link_text("Logout").click()
 
-    def create_address_book(self, group):
+    def create_address_book(self, contact):
         self.open_address_book_page()
         wd = self.wd
         # fill in the form
         wd.find_element_by_name("firstname").click()
         wd.find_element_by_name("firstname").clear()
-        wd.find_element_by_name("firstname").send_keys(group.firstname)
+        wd.find_element_by_name("firstname").send_keys(contact.firstname)
         wd.find_element_by_name("lastname").click()
         wd.find_element_by_name("lastname").clear()
-        wd.find_element_by_name("lastname").send_keys(group.lastname)
+        wd.find_element_by_name("lastname").send_keys(contact.lastname)
         wd.find_element_by_name("title").click()
         wd.find_element_by_name("title").clear()
-        wd.find_element_by_name("title").send_keys(group.title)
+        wd.find_element_by_name("title").send_keys(contact.title)
         wd.find_element_by_name("company").click()
         wd.find_element_by_name("company").clear()
-        wd.find_element_by_name("company").send_keys(group.company)
+        wd.find_element_by_name("company").send_keys(contact.company)
         wd.find_element_by_name("address").click()
         wd.find_element_by_name("address").clear()
-        wd.find_element_by_name("address").send_keys(group.address)
+        wd.find_element_by_name("address").send_keys(contact.address)
         wd.find_element_by_name("home").click()
         wd.find_element_by_name("home").clear()
-        wd.find_element_by_name("home").send_keys(group.homephone)
+        wd.find_element_by_name("home").send_keys(contact.homephone)
         wd.find_element_by_name("mobile").click()
         wd.find_element_by_name("mobile").clear()
-        wd.find_element_by_name("mobile").send_keys(group.cellphone)
+        wd.find_element_by_name("mobile").send_keys(contact.cellphone)
         wd.find_element_by_name("email").click()
         wd.find_element_by_name("email").clear()
-        wd.find_element_by_name("email").send_keys(group.email)
+        wd.find_element_by_name("email").send_keys(contact.email)
         # submit the form
         wd.find_element_by_xpath("//div[@id='content']/form/input[21]").click()
 
