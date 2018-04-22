1f87f58bd45715c8a2e12b819f19111d3a76f167
Andrey <avizovskiy@yahoo.com>
2018-04-21 22:22:35 -0700

    Added modify contact test

--- fixture/contact.py
+++ fixture/contact.py
@@ -42,6 +42,39 @@ class ContactHelper:
         wd.find_element_by_xpath(".//*[@id='content']/form[2]/div[2]/input").click()
         wd.switch_to_alert().accept()
 
+    def modify_first_contact(self, contact):
+        wd = self.app.wd
+        self.open_home_page()
+        wd.find_element_by_xpath(".//*[@id='maintable']/tbody/tr[2]/td[8]/a/img").click()
+        # modify contact form
+        wd.find_element_by_name("firstname").click()
+        wd.find_element_by_name("firstname").clear()
+        wd.find_element_by_name("firstname").send_keys(contact.firstname)
+        wd.find_element_by_name("lastname").click()
+        wd.find_element_by_name("lastname").clear()
+        wd.find_element_by_name("lastname").send_keys(contact.lastname)
+        wd.find_element_by_name("title").click()
+        wd.find_element_by_name("title").clear()
+        wd.find_element_by_name("title").send_keys(contact.title)
+        wd.find_element_by_name("company").click()
+        wd.find_element_by_name("company").clear()
+        wd.find_element_by_name("company").send_keys(contact.company)
+        wd.find_element_by_name("address").click()
+        wd.find_element_by_name("address").clear()
+        wd.find_element_by_name("address").send_keys(contact.address)
+        wd.find_element_by_name("home").click()
+        wd.find_element_by_name("home").clear()
+        wd.find_element_by_name("home").send_keys(contact.homephone)
+        wd.find_element_by_name("mobile").click()
+        wd.find_element_by_name("mobile").clear()
+        wd.find_element_by_name("mobile").send_keys(contact.cellphone)
+        wd.find_element_by_name("email").click()
+        wd.find_element_by_name("email").clear()
+        wd.find_element_by_name("email").send_keys(contact.email)
+        # submit the form
+        wd.find_element_by_xpath(".//*[@id='content']/form[1]/input[22]").click()
+
+
     def open_address_book_page(self):
         wd = self.app.wd
         wd.find_element_by_link_text("add new").click()
---
+++ test/modify_address_book.py
@@ -0,0 +1,8 @@
+from model.contact import Contact
+
+
+def test_modify_contact(app):
+    app.session.login(username="admin", password="secret")
+    app.contact.modify_first_contact(Contact(firstname="John_modified", lastname="Smith_modified", title="QA Engineer_modified", company="AAA_modified", address="123 main street_modified", homephone="123456_modified", cellphone="1234567_modified",
+                                        email="modified_test@mail.com"))
+    app.session.logout()
