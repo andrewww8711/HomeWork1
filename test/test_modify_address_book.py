b26b1d5aa786479e5961c41c8b91bff4d8b2d0d7
Andrey <avizovskiy@yahoo.com>
2018-05-01 21:23:54 -0700

    Задание 9: добавил проверку предусловий для тестов модификации.

--- test/test_modify_address_book.py
+++ test/test_modify_address_book.py
@@ -2,8 +2,12 @@ from model.contact import Contact
 
 
 def test_modify_contact_name(app):
+    if app.contact.count() == 0:
+        app.contact.create_address_book(Contact(firstname="test"))
     app.contact.modify_first_contact(Contact(firstname="John_modified", lastname="Smith_modified"))
 
 
 def test_modify_contact_address(app):
+    if app.contact.count() == 0:
+        app.contact.create_address_book(Contact(firstname="test"))
     app.contact.modify_first_contact(Contact(address="1111 California Street"))
--- test/test_modify_group_two.py
+++ test/test_modify_group_two.py
@@ -2,8 +2,11 @@ from model.group import Group
 
 
 def test_modify_group_name(app):
+    if app.group.count() == 0:
+        app.group.create(Group(name="test"))
     app.group.modify_first_group_two(Group(name="new group"))
 
-
 def test_modify_group_header(app):
+    if app.group.count() == 0:
+        app.group.create(Group(name="test"))
     app.group.modify_first_group_two(Group(header="new header"))
