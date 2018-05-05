ff0445ce8687c637a0a49f749bdbb4b10a3d77c9
Andrey <avizovskiy@yahoo.com>
2018-05-05 14:36:20 -0700

    Группы - загружаем инфу в список

--- test/test_create_groups.py
+++ test/test_create_groups.py
@@ -3,8 +3,13 @@ from model.group import Group
 
 
 def test_homeworkone(app):
+        old_groups = app.group.get_group_list()
         app.group.create(Group(name="group name", header="header name", footer="footer name"))
-
+        new_groups = app.group.get_group_list()
+        assert len(old_groups) + 1 == len(new_groups)
 
 def test_empty_group_homeworkone(app):
+        old_groups = app.group.get_group_list()
         app.group.create(Group(name="", header="", footer=""))
+        new_groups = app.group.get_group_list()
+        assert len(old_groups) + 1 == len(new_groups)
