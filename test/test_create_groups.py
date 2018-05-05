538e4479336933cc6640ddf0c59b241a6c24cc48
Andrey <avizovskiy@yahoo.com>
2018-05-05 16:02:24 -0700

    Группы - Реализовал проверки в виде сравнения списков

--- test/test_create_groups.py
+++ test/test_create_groups.py
@@ -4,12 +4,20 @@ from model.group import Group
 
 def test_homeworkone(app):
         old_groups = app.group.get_group_list()
-        app.group.create(Group(name="group name", header="header name", footer="footer name"))
+        group = Group(name="group name", header="header name", footer="footer name")
+        app.group.create(group)
+        #app.group.create(Group(name="group name", header="header name", footer="footer name"))
         new_groups = app.group.get_group_list()
         assert len(old_groups) + 1 == len(new_groups)
+        old_groups.append(group)
+        assert sorted(old_groups, key=Group.id_id_max) == sorted(new_groups, key=Group.id_id_max)
 
-def test_empty_group_homeworkone(app):
-        old_groups = app.group.get_group_list()
-        app.group.create(Group(name="", header="", footer=""))
-        new_groups = app.group.get_group_list()
-        assert len(old_groups) + 1 == len(new_groups)
+
+#def test_empty_group_homeworkone(app):
+        #old_groups = app.group.get_group_list()
+        #group = Group(name="", header="", footer="")
+        #app.group.create(group)
+        #new_groups = app.group.get_group_list()
+        #assert len(old_groups) + 1 == len(new_groups)
+        #old_groups.append(group)
+        #assert sorted(old_groups, key=Group.id_id_max) == sorted(new_groups, key=Group.id_id_max)
