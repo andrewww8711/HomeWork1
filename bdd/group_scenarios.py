9c03b7c78713b1c4fab7650eb5d9adb99f1ad27e
Andrey <avizovskiy@yahoo.com>
2018-07-07 23:52:16 -0700

    BDD

--- bdd/group_scenarios.py
+++ bdd/group_scenarios.py
@@ -3,6 +3,10 @@
 from pytest_bdd import scenario
 from .group_steps import *
 
-@scenario('group.feature', 'Add new group')
+@scenario('groups.feature', 'Add new group')
 def test_add_new_group():
+    pass
+
+@scenario('groups.feature', 'Delete a group')
+def test_delete_group():
     pass
--- bdd/group_steps.py
+++ bdd/group_steps.py
@@ -1,6 +1,7 @@
 
 from pytest_bdd import given, when, then
 from model.group import Group
+import random
 
 @given('a group list')
 def group_list(db):
@@ -15,8 +16,34 @@ def add_new_group(app, new_group):
     app.group.create(new_group)
 
 @then('the new group list is equal to the old list with the added group')
-def verify_group_added(db, group_list, new_group):
+def verify_group_added(db, group_list, new_group, app):
     old_groups = group_list
     new_groups = db.get_group_list()
     old_groups.append(new_group)
     assert sorted(new_groups, key=Group.id_id_max) == sorted(app.group.get_group_list(), key=Group.id_id_max)
+
+@given('a non-empty group list')
+def non_empty_group_list(db, app):
+    if len(db.get_group_list()) == 0:
+        app.group.create(Group(name="test"))
+    return db.get_group_list()
+
+@given('a random group from the list')
+def random_group(non_empty_group_list):
+    return random.choice(non_empty_group_list)
+
+@when('I delete the group from the list')
+def delete_group(app, random_group ):
+    app.group.delete_group_by_id(random_group.id)
+
+@then('the new group list is equal to the old list with the added group')
+def verify_group_deleted(db, non_empty_group_list, random_group, app, check_ui ):
+    old_groups = non_empty_group_list
+    new_groups = db.get_group_list()
+    assert len(old_groups) - 1 == app.group.count()
+    old_groups.remove(random_group)
+    assert old_groups == new_groups
+    if check_ui:
+        assert sorted(new_groups, key=Group.id_id_max) == sorted(app.group.get_group_list(), key=Group.id_id_max)
+
+
